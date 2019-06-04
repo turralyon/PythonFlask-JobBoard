a4d1bb55161d644ee9f4720459b025a2a15fecb0
turralyon <38249920+turralyon@users.noreply.github.com>
2019-06-04 13:34:37 +0200

    first commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,6 @@
 import datetime
 import sqlite3
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, redirect, url_for
 
 PATH = 'db/jobs.sqlite'
 
@@ -71,4 +71,5 @@ def review(employer_id):
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
